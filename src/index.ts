e9b27b6089adc7ab85a3723d3720f54cd0b54ad5
Adriel <adrielalmeidap@gmail.com>
2022-01-11 14:58:24 -0300

    Commit init graphql-api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import { Post, prisma, User } from "@prisma/client";
 import { ApolloServer } from "apollo-server";
 import { DateTimeResolver } from "graphql-scalars";
+import { title } from "process";
 import { Context, context } from "./context";
 
 const typeDefs = `
@@ -40,12 +42,20 @@ scalar DateTime
 
 const resolvers = {
   Query: {
+    //Busca todos os objetos do tipo User
     allUsers: (_parent, _args, context: Context) => {
-      // TODO
+      return context.prisma.user.findMany();
     },
+    //Busca uma postagem por ID
     postById: (_parent, args: { id: number }, context: Context) => {
-      // TODO
+      return context.prisma.post.findUnique({
+        where: {
+          id: args.id
+        }
+      })
     },
+    //Busca todas as postagens publicadas. Quando receber os parâmetros skip e take, faça paginação. 
+    //Quando receber o parâmetro searchString, pesquise a string no título ou conteúdo da postagem.
     feed: (
       _parent,
       args: {
@@ -55,46 +65,105 @@ const resolvers = {
       },
       context: Context
     ) => {
-      // TODO
+      return context.prisma.post.findMany({
+        where: {
+          published: true,
+          OR: [
+            { title:   { contains: args.searchString as string } },
+            { content: { contains: args.searchString as string } }
+          ]
+        },
+        skip: Number(args.skip) || undefined,
+        take: Number(args.take) || undefined,
+      })
     },
+    //Busca as postagens não publicadas de uma pessoa específica
     draftsByUser: (_parent, args: { id: number }, context: Context) => {
-      // TODO
+      return context.prisma.post.findMany({
+        where: {
+          published: false,
+          authorId: args.id,
+        }
+      })
+    },
+    posts: (_parent, args: { id: number }, context: Context) => {
+      return context.prisma.user.findUnique({
+        where: {
+          id: 
+        }
+      })
     },
   },
   Mutation: {
+    //Cria um objeto do tipo User
     signupUser: (
       _parent,
       args: { name: string | undefined; email: string },
       context: Context
     ) => {
-      // TODO
+      return context.prisma.user.create({
+        data: {
+          email: args.email,
+          name: args.name,
+        }
+      })
     },
+    //Cria um objeto do tipo Post
     createDraft: (
       _parent,
       args: { title: string; content: string | undefined; authorEmail: string },
       context: Context
     ) => {
-      // TODO
+      return context.prisma.post.create({
+        data: {
+          title:   args.title,
+          content: args.content,
+          author: {
+            connect: {
+              email: args.authorEmail,
+            }
+          }
+        }
+      })
     },
+    //Aumenta as visualizações de uma postagem em 1
     incrementPostViewCount: (
       _parent,
       args: { id: number },
       context: Context
     ) => {
-      // TODO
+      return context.prisma.post.update({
+        data: {
+          viewCount: {
+            increment: 1
+          }
+        },
+        where: { id: Number(args.id) }
+      })
     },
+    //Deleta uma postagem
     deletePost: (_parent, args: { id: number }, context: Context) => {
-      // TODO
+      return context.prisma.post.delete({
+        where: { id: args.id }
+      })
     },
   },
   Post: {
-    author: (parent, _args, context: Context) => {
-      return null;
+    author: (parent: Post, _args, context: Context) => {
+      return context.prisma.user.findUnique({
+        where: {
+          id: Number(parent.authorId)
+        }
+      });
     },
   },
   User: {
-    posts: (parent, _args, context: Context) => {
-      return [];
+    posts: (parent: User, _args, context: Context) => {
+      return context.prisma.post.findMany({
+        where: {
+          authorId: parent.id,
+        }
+      })
     },
   },
   DateTime: DateTimeResolver,
